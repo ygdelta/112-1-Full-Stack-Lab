7aaf7eb17622732ab3950ec608f19114137f4eb3
ChunWang30832 <84301277+ChunWang30832@users.noreply.github.com>
2023-12-21 15:26:50 +0800

    SQLite撈資料練習+測試
    更新DB Table
    撈資料測試

--- server.js
+++ server.js
@@ -8,19 +8,61 @@ var bodyParser = require('body-parser');
 var db = new sqlite3.Database('db/test.db');
 const app = express();
 
-db.each('SELECT * FROM student', function(err, row) {
-  if (err) return console.log(err.message);
-  console.log(row.ID + ':' + row.Name+" "+ row.Subject);
-});
-db.close();
+// db.each('SELECT * FROM student', function (err, row) {
+//   if (err) return console.log(err.message);
+//   console.log(row.ID + ':' + row.Name + " " + row.Subject);
+// });
+// db.close();
+/////////////////////////////12/21練習的領域/////////////////////////////////////////
+// SQL查詢學生有哪些課
+const query = `
+    SELECT Class.ID, Class.Name as ClassName
+    FROM User
+    JOIN StoC_relation ON User.ID = StoC_relation.UserID
+    JOIN Class ON StoC_relation.ClassID = Class.ID
+    WHERE User.Name = ?;
+`;
+// SQL查詢課的老師是誰
+const query2 = `
+    SELECT User.ID as TeacherID, User.Name as TeacherName, Class.Name as ClassName
+    FROM Class
+    JOIN User ON User.ID = Class.TeacherID
+    WHERE Class.Name = ?;
+`;
+// SQL查詢老師有哪些課
+const query3 = `
+    SELECT User.ID as TeacherID, User.Name as TeacherName, Class.ID as ClassID, Class.Name as ClassName
+    FROM User
+    JOIN TtoC_relation ON User.ID = TtoC_relation.TeacherID
+    JOIN Class ON TtoC_relation.ClassID = Class.ID
+    WHERE User.Name = ?;
+`;
+//測試用
+const targetUsername = '蔡明誠';
+// 執行查詢
+db.all(query3, targetUsername, (err, rows) => {
+  if (err) {
+    throw err;
+  }
 
+  // 處理查詢結果
+  console.log(`Information about ${targetUsername} :`);
+  rows.forEach(row => {
+    //console.log(`Class ID: ${row.ID}, Class Name: ${row.ClassName}`);
+    //console.log(`Teacher ID: ${row.TeacherID}, Teacher Name: ${row.TeacherName}, Class Name: ${row.ClassName}`);
+    console.log(`Teacher ID: ${row.TeacherID}, Teacher Name: ${row.TeacherName}, Class ID: ${row.ClassID}, Class Name: ${row.ClassName}`);
+  });
+
+
+});
+/////////////////////////////////////////////////////////////////////////////////////
 // 設定靜態資源目錄
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/getclasses", function(req, res) {
+app.get("/getclasses", function (req, res) {
   const ID = req.query.ID;
   const Name = req.query.Name;
 
@@ -32,7 +74,7 @@ app.get("/getclasses", function(req, res) {
 
 });
 
-app.post("/postdata", function(req, res) {
+app.post("/postdata", function (req, res) {
   // 從 request body 中獲取 JSON 數據
   const data = req.body;
 
@@ -44,7 +86,7 @@ app.post("/postdata", function(req, res) {
 });
 
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   // 讀取 index.html 的內容
   const filePath = path.join(__dirname, "public", "static", "index.html");
 
@@ -55,7 +97,7 @@ app.get("/", function(req, res) {
       res.status(500).send("Internal Server Error");
     } else {
       // 成功讀取，回傳 HTML 內容
-      res.set("Content-Type","text/html");
+      res.set("Content-Type", "text/html");
       res.status(200).send(data);
     }
   });
@@ -69,7 +111,7 @@ app.get("/login", (req, res) => {
       console.error("Error reading button1.html:", err);
       res.status(500).send("Internal Server Error");
     } else {
-      res.set("Content-Type","text/html");
+      res.set("Content-Type", "text/html");
       res.status(200).send(data);
     }
   });
@@ -83,7 +125,7 @@ app.get("/register", (req, res) => {
       console.error("Error reading button2.html:", err);
       res.status(500).send("Internal Server Error");
     } else {
-      res.set("Content-Type","text/html");
+      res.set("Content-Type", "text/html");
       res.status(200).send(data);
     }
   });
