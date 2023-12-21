53be63fd485cf16e41734439eca96cde7504cf86
ygdelta <yt91205@gmail.com>
2023-12-21 19:09:07 +0800

    留言板功能未完成
    To do:
    1. [Fix] 嵌入式影片
    2. 留言板

--- public/js/index.js
+++ public/js/index.js
@@ -11,6 +11,11 @@ const components = {
     "commentCard": "static/template/message_area.html"
 };
 
+let userData = {
+    name: "testUser",
+    type: "student"
+};
+
 $(document).ready(function () {
     let content = $("#content");
     let sidebar = $("#sidebar");
@@ -82,10 +87,65 @@ function ShowPage(page, container = $("#content")) {
     });
 }
 
-function formatDate(date = new Date()) {
+function FormatDate(date = new Date()) {
     const year = date.toLocaleString("default", {year: "numeric"});
     const month = date.toLocaleString("default", {month: "2-digit"});
     const day = date.toLocaleString("default", {day: "2-digit"});
     return year + month + day;
     //return [year, month, day].join("-");
+}
+
+function SendComment(event) {
+    let idElement = $(event.currentTarget).parent().parent().children("p.message-area-id");
+    let id = idElement.text();
+    let comment = $(event.currentTarget).parent().children("input").val();
+    let data = {
+        id: id,
+        content: {
+            user: userData.name,
+            comment: comment,
+            date: new Date()
+        }
+    };
+    
+    var commentTemplate = [
+        '<div class="comment flex items-start mt-4">',
+            '<img class="w-11 h-11" src="../../img/user.png">',
+            '<div class="flex flex-col items-start">',
+                '<div class="flex flex-row items-start justify-center h-fit mx-3">',
+                    '<p class="text-sm font-bold">${content.user}</p>',
+                    '<p class="text-xs text-stone-400 mx-2">${content.date}</p>',
+                '</div>',           
+                '<p class="text-wrap text-left text-base mx-3">${content.comment}</p>',
+            '</div>',
+        '</div>'];
+
+    $.ajax({
+        type: "POST",
+        url: "SendComment",
+        data: data,
+        dataType: "JSON",
+    })
+    .then(function(res) {
+        /* Wait implementation */
+    }, function(err) {
+        alert("Error occurred!");
+        /* location.reload(); */
+        let comment = `<div class="comment flex items-start mt-4">
+            <img class="w-11 h-11" src="../../img/user.png">
+            <div class="flex flex-col items-start">
+                <div class="flex flex-row items-start justify-center h-fit mx-3">
+                    <p class="text-sm font-bold">${data.content.user}</p>
+                    <p class="text-xs text-stone-400 mx-2">${FormatDate(data.content.date)}</p>
+                </div>           
+                <p class="text-wrap text-left text-base mx-3">${data.content.comment}</p>
+            </div>
+        </div>`;
+        let comments = $("div.comment");
+        if(comments.length != 0)
+            $(comments[comments.length - 1]).addClass("hidden");
+        $(comment).appendTo(".message-area-main");
+    });
+
+    $(event.currentTarget).parent().children("input").val("");
 }
