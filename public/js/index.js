efbb535d4d503bc48840439b19e1487e38266efb
ygdelta <yt91205@gmail.com>
2023-12-24 05:11:02 +0800

    完成老師新增課程內容
    To do:
    1. 依身分不同進行按鈕隱藏
    2. 登入後顯示使用者名稱與登出

--- public/js/index.js
+++ public/js/index.js
@@ -16,35 +16,56 @@ let userData = {
     type: "teacher"
 };
 
+//<!--html-->
 const joinClassTemplate = `
-    <!--html-->
     <div class="flex items-center justify-center w-full f-fit">
         <div class="flex flex-row items-center w-full">
             <input class="text-lg classic-border rounded-md w-full h-fit p-3" type="number" placeholder="請輸入課程代碼"> 
             <button onclick="OnJoinClass(event)" class="text-lg text-white p-2 bg-blue-600 rounded-md w-24 h-fit m-2">新增</button>
         </div>
     </div>
-    <!--!html-->
 `;
+//<!--!html-->
 
+//<!--html-->
 const createClassTemplate = `
-    <!--html-->
     <div class="flex items-center justify-center w-full f-fit">
         <div class="flex flex-row items-center w-full">
             <input class="text-lg classic-border rounded-md w-full h-fit p-3" type="number" placeholder="請輸入課程名稱"> 
-            <button onclick="OnJoinClass(event)" class="text-lg text-white p-2 bg-blue-600 rounded-md w-24 h-fit m-2">建立</button>
+            <button onclick="OnCreateClass(event)" class="text-lg text-white p-2 bg-blue-600 rounded-md w-24 h-fit m-2">建立</button>
         </div>
     </div>
-    <!--!html-->
 `;
+//<!--!html-->
+
+//<!--html-->
+const createChapterTemplate = `
+    <div class="flex items-center justify-center w-full f-fit">
+        <div class="flex flex-row items-center w-full">
+            <input class="text-lg classic-border rounded-md w-full h-fit p-3" type="text" placeholder="請輸入章節名稱"> 
+            <button onclick="OnCreateChapter(event)" class="text-lg text-white p-2 bg-blue-600 rounded-md w-24 h-fit m-2">建立</button>
+        </div>
+    </div>
+`;
+//<!--!html-->
+
+//<!--html-->
+const createSectionTemplate = `
+    <div class="flex flex-col items-center justify-center w-full f-fit">
+        <input class="text-lg classic-border rounded-md w-full h-fit mb-2 p-3" type="text" placeholder="請輸入小節名稱"> 
+        <input class="text-lg classic-border rounded-md w-full h-fit mb-2 p-3" type="text" placeholder="請輸入Youtube影片網址"> 
+        <button onclick="OnCreateSection(event)" class="text-lg classic-border rounded-md w-full h-fit">新增</button>
+    </div>
+`;
+//<!--!html-->
 
 $(document).ready(function () {
+    $("#class-id").text("");
     let content = $("#content");
     let sidebar = $("#sidebar");
 
     // For Debug
-    // ShowPage("home");
-    ShowPage("class");
+    ShowPage("home");
 
     sidebar.hover(function() {
         if(sidebar.data("isOpen")) {
@@ -86,11 +107,23 @@ $(document).ready(function () {
     });
 
     $("#home-btn").on("click", function(e) {
+        $("#class-id").text("");
         ShowPage("home");
     });
+
+    $()
 });
 
 // Funcitons
+function GetIDfromURL(url) {
+    const regExp = 
+      /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/|shorts\/|live\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
+    const match = url.match(regExp);
+    if (match && match[1].length === 11)
+      return match[1];
+    return '';
+}
+
 function ReWriteCss(element, cssStyle, property) {
     element.css(cssStyle, property);
 }
@@ -176,11 +209,12 @@ function SendComment(event) {
 
 function ShowModal(setting = {
     title: "Title",
-    content: "Content"
+    content: "Content",
 }) {
     let body = $("body");
+
+    //<!--html-->
     let template = `
-    <!--html-->
     <div id="modal" class="fixed top-0 left-0 z-[49]">
         <div onclick="CloseModal()" class="fixed top-0 left-0 bg-gray-800 opacity-50 w-screen h-screen z-[49]"></div>
         <div class="flex flex-col fixed top-[calc(50%-150px)] left-[calc(50%-250px)] bg-stone-50 classic-border rounded-md w-[500px] h-[300px] z-50">
@@ -188,8 +222,8 @@ function ShowModal(setting = {
             <div class="flex w-full h-full px-2 pb-3">${setting.content}</div>
         </div>
     </div>
-    <!--!html-->
     `;
+    //<!--!html-->
     $.tmpl(template, {}).appendTo(body);
 }
 
@@ -227,5 +261,97 @@ function OnCreateClass(e) {
         CloseModal();
     }, function(err) {
         alert("建立課程發生錯誤");
+    });
+}
+
+function OnCreateChapter(e) {
+    let chapterName = $(e.currentTarget).prev().val();
+    if(chapterName == "") {
+        alert("請輸入章節名稱");
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "CreateChapter",
+        data: {name: chapterName},
+        dataType: "dataType",
+    })
+    .then(function(res) {
+        //<!--html-->
+        let chapterTitle = `
+        <div class="flex flex-col w-full">
+            <div class="flex justify-between items-center border-solid border-blue-600 border-b w-full p-3">
+                <p class="hidden">${id}</p>
+                <div class="text-left text-3xl text-blue-600">${chapterName}</div>
+                <div class="flex h-fit w-fit">
+                    <svg onclick="ShowModal({title: '新增小節', content: createSectionTemplate})" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block rounded-full w-6 h-6 hover:cursor-pointer hover:bg-stone-300">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                    <div class="w-1 mx-2"></div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block rounded-full w-6 h-6 hover:cursor-pointer hover:bg-stone-300">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+        `;    
+        //<!--!html-->
+
+        $.tmpl(chapterTitle, res.data).insertBefore("#btn-add-chapter");
+        CloseModal();
+    }, function(err) {
+        //<!--html-->
+        let chapterTitle = `
+        <div class="flex flex-col w-full">
+            <div class="flex justify-between items-center border-solid border-blue-600 border-b w-full p-3">
+                <p class="hidden">testId</p>
+                <div class="text-left text-3xl text-blue-600">${chapterName}</div>
+                <div class="flex h-fit w-fit">
+                    <svg onclick="ShowModal({title: '新增小節', content: createSectionTemplate})" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block rounded-full w-6 h-6 hover:cursor-pointer hover:bg-stone-300">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                    <div class="w-1 mx-2"></div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block rounded-full w-6 h-6 hover:cursor-pointer hover:bg-stone-300">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+        `;    
+        //<!--!html-->
+
+        $.tmpl(chapterTitle, {}).insertBefore("#btn-add-chapter");
+        CloseModal();
+    });
+}
+
+function OnCreateSection(e) {
+    let sectionName = $(e.currentTarget).prev().prev().val();
+    let videoId = GetIDfromURL($(e.currentTarget).prev().val());
+    if(sectionName == "") {
+        alert("請輸入小節名稱");
+        return;
+    }
+    if(videoId == "") {
+        alert("影片URL有誤");
+        return;
+    }
+    $.ajax({
+        type: "POST",
+        url: "CreateSection",
+        data: {
+            name: sectionName,
+            videoId: videoId
+        },
+        dataType: "JSON",
+    })
+    .then(function(res) {
+        ShowPage("class/information", $("#class-content"));
+        CloseModal();
+    }, function(err) {
+        alert("新增小節發生錯誤");
+        ShowPage("class/information", $("#class-content"));
+        CloseModal();
     });
 }
